61bb1c9b0653b93dceb0d77ef9473efeed4b43f3
ArolaunTech <aadityasenji@gmail.com>
2025-09-23 21:26:26 -0700

    FFmpeg actually works now

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
-#include <stdio.h>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 extern "C" {
 	#include <libavcodec/avcodec.h>
-	#include <libavformat/avformat.h>
-	#include <libavutil/imgutils.h>
-	#include <libavutil/avutil.h>
-	#include <libswscale/swscale.h>
 }
 
 #include "scene/scene.h"
 #include "project.h"
 
 int main() {
+	const AVCodec* encoder = avcodec_find_encoder(AV_CODEC_ID_H265);
+	if (!encoder) {
+		std::cerr << "Encoder not initialized.\n";
+		return 1;
+	}
+
 	Scene mainscene;
 	setup(mainscene);
 
 	if (!glfwInit()) {
-		std::cout << "Initialization of glfw failed.\n";
+		std::cerr << "Initialization of glfw failed.\n";
 		return 1;
 	}
 
@@ -28,7 +29,7 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	GLFWwindow* window = glfwCreateWindow(640, 480, "Preview", NULL, NULL);
 	if (!window) {
-		std::cout << "Window creation failed.\n";
+		std::cerr << "Window creation failed.\n";
 		glfwTerminate();
 		return 1;
 	}
